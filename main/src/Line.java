26c3cc0dd4aeefcd60ba2e6b75dff4557afc4592
yairo <yoffir@heed-global.com>
2018-06-18 17:52:36 +0300

    kata

--- main/src/Line.java
+++ main/src/Line.java
@@ -1,11 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Line {
+
     public static String Tickets(int[] peopleInLine)
     {
+        Register register = new Register();
         for (int buyer : peopleInLine){
-            if (buyer > 25){
-                return "NO";
+            if (buyer == 50){
+                if (register.haveBill(25)){
+                    register.takeBill(25);
+                } else {
+                    return "NO";
+                }
             }
+            if (buyer == 100){
+                if (register.haveBill(50) && register.haveBill(25)){
+                    register.takeBill(25);
+                    register.takeBill(50);
+                } else {
+                    return "NO";
+                }
+            }
+            register.addBill(buyer);
         }
         return "YES";
+    }
+
+    public static class Register {
+        private Map<Integer, Integer> amountByValue;
+
+        public Register(){ amountByValue = new HashMap<>(); }
+
+        public void addBill(int value){
+            Integer numOfBills = amountByValue.getOrDefault(value, 0);
+            amountByValue.put(value, ++numOfBills);
+        }
+
+        public boolean haveBill(int value){
+            Integer numOfBills = amountByValue.get(value);
+            return numOfBills != null && numOfBills != 0;
+        }
+
+        public void takeBill(int value){
+            Integer numOfBills = amountByValue.get(value);
+            amountByValue.put(value, --numOfBills);
+        }
+
+
     }
 }
--- main/src/LineTest.java
+++ main/src/LineTest.java
@@ -13,14 +13,30 @@ public class LineTest {
     public void onePersonBigBillNotOK() {
         assertEquals("NO", Line.Tickets(new int[] {50}));
     }
-//    @Test
-//    public void test1() {
-//        assertEquals("YES", Line.Tickets(new int[] {25, 25, 50}));
-//    }
-//
-//    @Test
-//    public void test2() {
-//        assertEquals("NO", Line.Tickets(new int []{25, 100}));
-//    }
+
+    @Test
+    public void twoPersonsFirstExactOK() {
+        assertEquals("YES", Line.Tickets(new int[] {25,50}));
+    }
+
+    @Test
+    public void shouldBeEnoughChangeFor50() {
+        assertEquals("YES", Line.Tickets(new int[] {25, 25, 50}));
+    }
+
+    @Test
+    public void NotEnoughChangeFor100() {
+        assertEquals("NO", Line.Tickets(new int []{25, 100}));
+    }
+
+    @Test
+    public void shouldHave50Bill25BillFor100() {
+        assertEquals("YES", Line.Tickets(new int []{25, 50, 25, 100}));
+    }
+
+    @Test
+    public void shouldHave3Bills25For100() {
+        assertEquals("YES", Line.Tickets(new int []{25, 25, 25, 100}));
+    }
 
 }
